d795ab3feada560275d24a28472c8d81abd6aca0
mansoor <codehawk64@gmail.com>
2014-09-09 18:30:29 +0530

    dfssda

--- Assets/objectmove.cs
+++ Assets/objectmove.cs
@@ -6,18 +6,21 @@ public class objectmove : MonoBehaviour {
 
 
 
+	Vector3 inipos;
+
 	// Use this for initialization
 	void Start () {
 	
+		inipos = this.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		if(this.transform.position.z>0)
-		this.transform.Translate(Vector3.back*0.3f);
-		else
-			this.transform.Translate(Vector3.forward*60);
+	//	if(this.transform.position.z>-10)
+		this.transform.Translate(Vector3.back*0.2f);
+	//	else
+	//		this.transform.Translate(Vector3.right*40);
 
 
 	}
